61cd19cce6739646657a3e4024174e4b424c208d
adikarthik <33541222+adikarthik@users.noreply.github.com>
2022-11-15 20:48:12 +0530

    Update deliver.sh

--- scripts/deliver.sh
+++ scripts/deliver.sh
@@ -1,16 +1,3 @@
 #!/usr/bin/env bash
 
-set -x
-mvn jar:jar install:install help:evaluate -Dexpression=project.name
-set +x
-
-set -x
-NAME=`mvn help:evaluate -Dexpression=project.name | grep "^[^\[]"`
-set +x
-
-set -x
-VERSION=`mvn help:evaluate -Dexpression=project.version | grep "^[^\[]"`
-set +x
-
-set -x
 java -jar target/my-app-1.0-SNAPSHOT.jar
